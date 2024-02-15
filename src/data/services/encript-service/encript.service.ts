afa6756a1618308930a79040681a8539080705a5
JersonBasto <90776343+JersonBasto@users.noreply.github.com>
2024-02-15 07:03:28 -0500

    cambios encriptador
    Se hace uso de JWT

--- src/data/services/encript-service/encript.service.ts
+++ src/data/services/encript-service/encript.service.ts
@@ -1,5 +1,5 @@
 import { Injectable } from '@angular/core';
-import { JwtHelperService } from '@auth0/angular-jwt';
+import * as CryptoJS from 'crypto-js';
 
 @Injectable({
   providedIn: 'root',
@@ -7,13 +7,43 @@ import { JwtHelperService } from '@auth0/angular-jwt';
 export class EncriptService {
   secret: string =
     '2ES2nH4J/hJf18sSLTh/X2JvXtOUo7NYX4irfCOTK+MWAz0S7xOzSF/KbhZRxE17e208EQaxAsXQkmG2YgxNmQ==';
-  constructor(private jwtHelper:JwtHelperService) {}
+  constructor() {}
 
-  generateToken(data: any): string {
-    const options: jwtHelper.SignOptions = {
-      expiresIn: '1h',
+  base64url(source: any) {
+    let encodedSource = CryptoJS.enc.Base64.stringify(source);
+    encodedSource = encodedSource.replace(/=+$/, '');
+    encodedSource = encodedSource.replace(/\+/g, '-');
+    encodedSource = encodedSource.replace(/\//g, '_');
+    return encodedSource;
+  }
+
+  encodeToken(payload: any) {
+    var header = {
+      alg: 'HS512', // Cambiado a HS512 para coincidir con Spring Boot
+      typ: 'JWT',
     };
-    const token = jwt.sign(data, this.secret, options);
+
+    var stringifiedHeader = CryptoJS.enc.Utf8.parse(JSON.stringify(header));
+    var encodedHeader = this.base64url(stringifiedHeader);
+
+    var stringifiedData = CryptoJS.enc.Utf8.parse(JSON.stringify(payload));
+    var encodedData = this.base64url(stringifiedData);
+
+    var token = encodedHeader + '.' + encodedData;
     return token;
+  }
+
+  signToken(payload: any) {
+    var signature: any = CryptoJS.HmacSHA512(payload, this.secret); // Cambiado a HmacSHA512
+    signature = this.base64url(signature);
+
+    var signedToken = payload + '.' + signature;
+    return signedToken;
+  }
+
+  generateToken(data: any) {
+    const token = this.encodeToken(data);
+    const signedToken = this.signToken(token);
+    console.log(signedToken);
   }
 }
